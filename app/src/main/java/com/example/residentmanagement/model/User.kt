0b14fb96894f3e9d7ce8b1e5bc4be63190c08d3a
Juan Fernando Angulo <juanferas25@hotmail.com>
2023-04-08 15:14:52 -0500

    LoginActivity added

---
+++ app/src/main/java/com/example/residentmanagement/model/User.kt
@@ -0,0 +1,9 @@
+package com.example.residentmanagement.model
+
+data class User (
+    var name: String,
+    var age: Int,
+    var email: String,
+    var address: String,
+    var phone: String
+    )
